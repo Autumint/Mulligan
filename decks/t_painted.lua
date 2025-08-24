f454a99126b99f8d02bbc7e40f7873acafe78096
Mint <dekiaki1212@gmail.com>
2025-08-24 23:23:25 +0200

    "Better Jokers" functionality for t. painted

--- decks/t_painted.lua
+++ decks/t_painted.lua
@@ -11,10 +11,23 @@ SMODS.Back{
     loc_txt = {
         name = "Dried Deck",
         text = {
-            "{C:White}Better Jokers.",
+            "Better {C:money}Jokers.{}",
             "{C:red,E:2}Dry Paint.{}"
         }
     },
+
+      apply = function(self)
+    G.GAME.common_mod   = (G.GAME.common_mod   or 1) * 0.8
+    G.GAME.uncommon_mod = (G.GAME.uncommon_mod or 1) * 1.5
+    G.GAME.rare_mod     = (G.GAME.rare_mod     or 1) * 2.0
+
+    G.GAME.banned_keys["j_card_sharp"] = true
+    G.GAME.banned_keys["j_troubadour"] = true
+    G.GAME.banned_keys["j_burglar"]    = true
+    G.GAME.banned_keys["v_grabber"]    = true
+    G.GAME.banned_keys["v_hieroglyph"] = true
+  end,
+
     calculate = function(self, back, context)
         if context.final_scoring_step then
             return { xmult = 2 }
@@ -26,11 +39,6 @@ SMODS.Back{
         elseif G.STATE ~= 8 then
             self._gave_money = false
         end
-
-        G.GAME.banned_keys["j_burglar"] = true
-        G.GAME.banned_keys["j_troubadour"] = true
-        G.GAME.banned_keys["v_grabber"] = true
-        G.GAME.banned_keys["v_hieroglyph"] = true
 
         if G.GAME and G.GAME.round_resets then
             if G.GAME.round_resets.hands > 1 then
