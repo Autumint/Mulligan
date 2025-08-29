6fa2debf6ef49633453817f064fa2d7d9369cf56
Mint <dekiaki1212@gmail.com>
2025-08-29 22:33:11 +0200

    Scuffed t. anaglyph patch. Proper doubled tag rewards without phantom tags. Functions fine with double tags as well.

--- decks/t_anaglyph.lua
+++ decks/t_anaglyph.lua
@@ -14,27 +14,47 @@ local skip_blind_ref = G.FUNCS.skip_blind
 G.FUNCS.skip_blind = function(e)
     if G.GAME and G.GAME.selected_back and G.GAME.modifiers.tainted_anaglyph then
         e.config.ref_table = e.UIBox:get_UIE_by_ID('select_blind_button').config.ref_table
-        local _tag = e.UIBox:get_UIE_by_ID('tag_container')
         G.GAME.skips = (G.GAME.skips or 0) + 1
-        if _tag then 
-          add_tag(_tag.config.ref_table, true)
+
+        
+        local tag_key
+        local _tag = e.UIBox:get_UIE_by_ID('tag_container')
+        if _tag and _tag.config and _tag.config.ref_table and _tag.config.ref_table.key then
+            tag_key = _tag.config.ref_table.key
         end
 
         G.GAME.tainted_anaglyph_scaled = true
 
-        return G.FUNCS.select_blind(e)
+        local ret = G.FUNCS.select_blind(e)
+        if tag_key then
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                func = function()
+                    local new_tag = Tag(tag_key)
+                    if new_tag then
+                        add_tag(new_tag)
+                    end
+                    return true
+                end
+            }))
+        end
+        if tag_key then
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                func = function()
+                    local new_tag = Tag(tag_key)
+                    if new_tag then
+                        add_tag(new_tag)
+                    end
+                    return true
+                end
+            }))
+        end
+
+        return ret
     end
     return skip_blind_ref(e)
 end
-
-local add_tag_ref = add_tag
-function add_tag(...)
-    if G.GAME and G.GAME.modifiers.tainted_anaglyph then
-        add_tag_ref(...)
-    end
-    return add_tag_ref(...)
-end
-
 local set_blind_ref = Blind.set_blind
 function Blind:set_blind(blind, reset, silent)
     set_blind_ref(self, blind, reset, silent)
