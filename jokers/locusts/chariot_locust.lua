97463e4aa86afeba1d9310156423b00f5dfaa8f8
Mint <dekiaki1212@gmail.com>
2025-09-26 21:34:39 +0200

    add the lost chariot locust

---
+++ jokers/locusts/chariot_locust.lua
@@ -0,0 +1,60 @@
+do
+    local original_use_and_sell = G.UIDEF.use_and_sell_buttons
+
+    local function remove_sell_button(node)
+        if not node or not node.nodes then return end
+        for i = #node.nodes, 1, -1 do
+            local child = node.nodes[i]
+            if child.config and child.config.button == "sell_card" then
+                table.remove(node.nodes, i)
+            else
+                remove_sell_button(child)
+            end
+        end
+    end
+
+    function G.UIDEF.use_and_sell_buttons(card)
+        local m = original_use_and_sell(card)
+        if card.config and card.config.center and card.config.center.key == "j_tdec_heavy_locust" then
+            remove_sell_button(m)
+        end
+        return m
+    end
+end
+
+SMODS.Joker {
+    key = "heavy_locust",
+    atlas = "tainted_atlas",
+    blueprint_compat = true,
+    perishable_compat = false,
+    rarity = 1,
+    cost = 0,
+    pos = { x = 0, y = 0 },
+    no_collection = true,
+    unlocked = true,
+    discovered = true,
+    config = { extra = { size = 1, mult = 1 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.size, card.ability.extra.mult } }
+    end,
+
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.mult * #G.hand.cards
+            }
+        end
+    end,
+
+    add_to_deck = function(self, card, from_debuff)
+        G.locust_area.config.card_limit = G.locust_area.config.card_limit + card.ability.extra.size
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        G.locust_area.config.card_limit = G.locust_area.config.card_limit - card.ability.extra.size
+    end,
+
+    in_pool = function(self)
+        return false
+    end,
+}
